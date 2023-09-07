0f16ba6c3a03bbfdda02f3c2d9bb5f1008f6d649
namrpand <namrpand@LIN51016967.corp.capgemini.com>
2023-09-07 11:56:28 +0530

    hi

---
+++ src/test/java/Scripts/TC4.java
@@ -0,0 +1,21 @@
+package Scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import PomPages.SkillraryLoginPage;
+import genericLibrary.BaseClass;
+
+public class TC4 extends BaseClass{
+
+	@Test
+	public void tc4() 
+	{
+		
+		
+		SkillraryLoginPage s = new SkillraryLoginPage(driver);
+		
+		
+	}
+
+}
